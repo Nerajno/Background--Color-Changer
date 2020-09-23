11bd30f349124f714c3dcbd79b2e632dae3bb0c9
nerajno <nerajno@gmail.com>
2020-09-22 21:19:28 -0400

    parts working

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // random color genBtn
  const rndmClrGen = () => {
      const randomColor = Math.floor(Math.random()*167777215).toString(16);
+     let test = "#"+randomColor;
+     console.log(test);
      document.body.style.backgroundColor = "#"+randomColor;
      color.innerHTML = "#"+randomColor;
  }
 
  //got help from => https://css-tricks.com/snippets/javascript/random-hex-color/
  randomColorGen.addEventListener("click", rndmClrGen);
- rndmClrGen();
