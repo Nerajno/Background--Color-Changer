3633ee3871dc21e985a4581b4f8757e4f9fd4e39
nerajno <nerajno@gmail.com>
2020-12-17 20:52:10 -0500

    trying to figure out 1 idea

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // random color genBtn
 const rndmClrGen = async () => {
-
   const randomColor = Math.random().toString(16).slice(2, 8);
   document.body.style.backgroundColor = "#" + randomColor;
-  const colorUrl = "https://www.thecolorapi.com/id?hex=" + randomColor + "&format=svg";
+  const colorUrl =
+    "https://www.thecolorapi.com/id?hex=" + randomColor + "&format=svg";
   try {
     const response = await fetch(colorUrl);
     const body = await response.text();
@@ -12,7 +12,6 @@ const rndmClrGen = async () => {
   } catch (error) {
     console.log(error);
   }
-
 };
 
 let rndnColor = rndmClrGen();
@@ -21,11 +20,17 @@ let rndnColor = rndmClrGen();
 
 randomColorGen.addEventListener("click", rndmClrGen);
 
-
 //TODO ~ Get value of input text
 //     ~ Fix the function or make a new function that has two different
 //       criteria and run either the random function or query the input.
 //  const textColorInput = document.getElementById("colorInput").value;
 //console.log(textColorInput);
+let searchInput = document.querySelector("#colorSearcher > form > input[type=submit]:nth-child(2)");
+ 
 
-
+// if submit btn clicked 
+//           run 1 function
+// else if random btn clicked 
+//      do other thing
+// else
+//    log this has issue
